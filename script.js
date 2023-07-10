e7ec80d6a47d603b040b46d4bc5f18d247e12df6
Marcel Schmidt <marcelschmidt1989@live.de>
2023-07-10 15:36:33 +0200

    Fixing same output bug and added button disable option

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 var id = document.querySelector(".advice span");
 var quote = document.querySelector(".quote");
+var button = document.querySelector(".button");
+var lastid;
+var disabled = false;
+
+document.addEventListener('click', () => {
+    if(disabled == false){
+        fetchapi();
+    }
+});
 
 fetchapi();
 
 async function fetchapi() {
+    disabled = true;
+    button.classList.add('disabled');
     fetch("https://api.adviceslip.com/advice")
         .then((response) => response.json())
         .then((data) => {
             const adviceId = data.slip.id;
             const advice = data.slip.advice;
-            id.innerHTML = adviceId;
-            quote.innerHTML = advice;
+            if (lastid == adviceId) {
+                fetchapi();
+            }
+            else{
+                id.innerHTML = adviceId;
+                quote.innerHTML = advice;
+                lastid = adviceId;
+                disabled = false;
+                button.classList.remove('disabled');
+            }    
         });
 }
